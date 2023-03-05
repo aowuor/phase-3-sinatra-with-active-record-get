28fc23470c9436146a390cc3aa76e94be0bb6f0d
aowuor <angelaowuor@gmail.com>
2023-03-05 17:28:33 +0300

    commit tree bd84cc452e46337617e4bb81cdfd380bcfaebb84

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,42 @@
 class ApplicationController < Sinatra::Base
+  set :default_content_type, 'application/json'
 
-  get '/' do
-    { message: "Hello world" }.to_json
+  get '/games' do
+    games = Game.all.order(:title).limit(10)
+    games.to_json
+  end
+
+  # get specific game attributes
+
+  # get '/games/:id' do
+  #   game = Game.find(params[:id])
+  #   game.to_json
+  # end
+
+   # get specific game attributes and its reviews
+
+  # get '/games/:id' do
+  #   game = Game.find(params[:id])
+  #   game.to_json(include: :reviews)
+  # end
+
+   # get specific game attributes and its reviews as well as users
+
+  # get '/games/:id' do
+  #   game = Game.find(params[:id])
+  #   game.to_json(include: { reviews: {include: :user } })
+  # end
+
+  # get specific attributes
+
+  get '/games/:id' do
+    game = Game.find(params[:id])
+    game.to_json(only: [:id, :title, :genre, :price], include: {
+      reviews: {only: [:comment, :score], include: {
+        user: {only: [:name] }
+      }}
+    }
+    )
   end
 
 end
